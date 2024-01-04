facb3f9d260a659226eba55900a51a687cf98694
Hunter N. Parente <hparente1999@gmail.com>
2024-01-04 17:40:16 -0600

    Added an additional variant of the 'banner.art' file called 'banner-slogan.art'. Completed prompt styling.

--- packages/cli/bin/axion.js
+++ packages/cli/bin/axion.js
@@ -1,14 +1,19 @@
 #!/usr/bin/env node
 import updateNotifier from 'update-notifier'
 import { Command } from 'commander'
+import Enquirer from 'enquirer'
+import chalk from 'chalk'
 
 // Local Imports
+import { promptIcons } from '../lib/prompts/promptStyles.js'
+import { promptColors } from '../lib/prompts/promptStyles.js'
 import { validateNodeVersion } from '../lib/utils/validateNodeVersion.js'
 import { logAxionBanner } from '../lib/utils/banners.js'
 import { cliPackage } from '../lib/utils/cliPackage.js'
 
 
 const notifier = updateNotifier({pkg: cliPackage})
+const { Select } = Enquirer
 
 validateNodeVersion()
 logAxionBanner()
@@ -21,6 +26,39 @@ const cliPackageVersion = cliPackage['version']
 program.name('Axion CLI')
     .description('CLI for Axion framework')
     .version(`${cliPackageVersion}`, '-v, --version', 'Outputs the current CLI version')
+
+
+const initCLISelect = new Select({
+    name: 'initCLISelect',
+    message: 'What would you like to do?',
+    symbols: {
+        pointer: {
+          on: chalk.hex(promptColors['primaryPurple']).visible(promptIcons['singleSelectPointer']),
+          off: ' '
+      }
+    },
+    choices: [
+        'Create a new Axion app',
+        'Build a custom theme for Axion'
+    ],
+    prefix(state) {
+        switch (state.status) {
+            case 'pending': return 'pending'
+            case 'cancelled': return 'cancelled'
+            case 'submitted': return 'submitted'
+        }
+    }
+})
+
+program.command('axion')
+    .alias('ax')
+    .description('Initializes Axion CLI')
+    .action(() => {
+        initCLISelect
+            .run()
+            .then(answer => console.log(answer))
+            .catch(console.error)
+    })
 
 program.command('create <appName>')
     .alias('c')
--- packages/cli/lib/utils/banners.js
+++ packages/cli/lib/utils/banners.js
@@ -3,6 +3,8 @@ import { dirname, join } from 'path'
 import { fileURLToPath } from 'url'
 import gradient from 'gradient-string'
 
+import { promptColors } from '../prompts/promptStyles.js'
+
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
@@ -31,52 +33,10 @@ export function logArtFromFile (filePath, operation = () => {}) {
 }
 
 export function logAxionBanner () {
-    const gradientColors = [
-        '#c06efa',
-        '#bc6df8',
-        '#b86bf6',
-        '#b46af3',
-        '#b068f1',
-        '#ac67ef',
-        '#a865ed',
-        '#a364ea',
-        '#9f62e8',
-        '#9b61e6',
-        '#975fe3',
-        '#935ee1',
-        '#8f5ddf',
-        '#8b5bdc',
-        '#875ada',
-        '#8358d8',
-        '#7f57d5',
-        '#7b55d3',
-        '#7754d1',
-        '#7352ce',
-        '#6f51cc',
-        '#6b50c9',
-        '#674ec7',
-        '#624dc5',
-        '#5e4bc2',
-        '#5a4ac0',
-        '#5648bd',
-        '#5247bb',
-        '#4d46b8',
-        '#4944b6',
-        '#4443b3',
-        '#4041b1',
-        '#3b40af',
-        '#363eac',
-        '#313daa',
-        '#2b3ca7',
-        '#253aa5',
-        '#1f39a2',
-        '#1737a0',
-        '#0d369d',
-    ]
-    const filePath = '../../assets/banner.art'
+    const filePath = '../../assets/banner-slogan.art'
     function bannerGradientOperation(line) {
         if (typeof line === 'string') {
-            return bannerGradient(gradientColors, line)
+            return bannerGradient(promptColors['bannerGradientColors'], line)
         }
     }
     logArtFromFile(filePath, bannerGradientOperation)
